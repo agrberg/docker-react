77bdab80c2d49ba77ad3aea0af5865414b2fb5e0
Aaron Rosenberg <aarongrosenberg@gmail.com>
2020-09-18 17:49:27 -0500

    testing workflow

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          OMG I'm working on AWS Elastic Beanstalk!!!
         </p>
         <a
           className="App-link"
